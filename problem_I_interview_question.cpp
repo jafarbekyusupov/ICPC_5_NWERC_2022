ef72bbea78204f6a296f915f0c127c6c3d177b5f
jafarbekyusupov <github.jafarbekyusupov@gmail.com>
2024-10-14 15:00:17 +0500

    NotFinished problem_I_interview_question.cpp

---
+++ problem_I_interview_question.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Buzz Words
+// Fizz - if i%A == 0
+// Buzz - if i%B == 0
+// FizzBuzz - if i%B == 0 && if i%A == 0
+
+int main(){
+    int start, end;
+    cin >> start >> end;
+    for(int i=start; i<=end; i++){
+        cin >> 
+    }
+    return 0;
+}
