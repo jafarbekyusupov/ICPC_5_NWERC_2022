537ece11731deb2b313af95ed30d0320821b2caa
jafarbekyusupov <github.jafarbekyusupov@gmail.com>
2024-10-14 15:03:40 +0500

    Create problem_I_interview_question.py

---
+++ problem_I_interview_question.py
@@ -0,0 +1,14 @@
+'''
+
+PROBLEM I - INTERVIEW QUESTIONS - FizzBuzz
+
+# Buzz Words
+# Fizz - if i%A == 0
+# Buzz - if i%B == 0
+# FizzBuzz - if i%B == 0 && if i%A == 0
+'''
+
+s, e = map(int, input.split())
+for(e-s+1){
+    if
+
